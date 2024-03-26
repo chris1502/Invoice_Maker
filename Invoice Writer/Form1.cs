feab063b7e46acfb25283313bc4b09d1b25eede5
chris1502 <thomas-chris96@outlook.com>
2024-03-26 14:11:23 +0000

    Complete before executable file

--- Invoice Writer/Form1.cs
+++ Invoice Writer/Form1.cs
@@ -1,15 +1,18 @@
-﻿using System;
+﻿
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace Invoice_Writer
 {
+
+
     public partial class Form1 : Form
     {
         public Form1()
